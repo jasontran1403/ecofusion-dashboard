ea4045f3ef348dd50c550f8b55e5f73be5da256c
jasontran1403 <71239283+jasontran1403@users.noreply.github.com>
2024-10-20 08:56:31 +0700

    update

--- js/data-table/data-table-act.js
+++ js/data-table/data-table-act.js
@@ -1,8 +1,21 @@
 (function ($) {
- "use strict";
+	"use strict";
 	
 	$(document).ready(function() {
-		 $('#data-table-basic').DataTable();
+	  $('#data-table-basic').DataTable({
+		// Disable the length menu
+		lengthMenu: [10, 25, 50, 100], // You can keep this to allow changing the page size if needed
+		pageLength: 10, // Default entries to show
+		// Hide the information display
+		info: false,
+		// Hide the entries dropdown
+		pagingType: "simple", // Optional: Adjust pagination style if needed
+		// To hide the entries showing info
+		language: {
+		  lengthMenu: '',
+		  info: ''
+		}
+	  });
 	});
- 
-})(jQuery); 
+  })(jQuery);
+  
